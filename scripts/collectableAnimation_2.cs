ba2a5bc7188572409c2ab59254f660133d1d0242
cottremi <49172056+cottremi@users.noreply.github.com>
2019-05-15 12:42:55 -0600

    Add files via upload

---
+++ scripts/collectableAnimation_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableAnimation : MonoBehaviour
+{
+
+	public float verticalSpeed;
+	public float amplitude;
+
+	public Vector3 tempPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       tempPosition = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed)* amplitude + 1;
+        transform.position = tempPosition;
+        transform.Rotate (new Vector3 (0, 0, 30) * Time.deltaTime);
+    }
+}
